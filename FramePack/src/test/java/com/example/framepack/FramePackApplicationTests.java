64707f4b4f545a3530fda1dcbe17b7aa160bc56e
luckysheep <2980419682@qq.com>
2024-12-02 02:18:30 +0800

    Add files via upload

---
+++ FramePack/src/test/java/com/example/framepack/FramePackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.framepack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FramePackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
